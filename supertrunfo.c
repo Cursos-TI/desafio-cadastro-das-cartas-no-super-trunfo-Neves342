043c973681473c2a337055f8f884b9037329c868
Neves342 <five21060@gmail.com>
2025-05-06 22:07:45 +0000

    supertrunfo_intermediário

--- supertrunfo.c
+++ supertrunfo.c
@@ -11,6 +11,8 @@ int main() {
        float area = 1521.11;
        double PIB = 699.28;
        int Numero_de_Pontos_Turisticos = 50;
+       float Densidade_Populacional = Populacao / area;
+       float PIB_per_Capita = PIB * 1000000000 / Populacao; 
        
        printf("Estado: %c \n", Estado);
        
@@ -25,6 +27,10 @@ int main() {
        printf("PIB: %.2lf bilhoes de reais \n", PIB);
        
        printf("Numero de Pontos Turisticos: %d \n", Numero_de_Pontos_Turisticos);
+
+       printf("Densidade Populacional: %.2f hab/km²\n", Densidade_Populacional);
+
+       printf("PIB per Capita: %.2f reais\n", PIB_per_Capita);
    }
      printf("\n \n");
 //Segunda carta
@@ -36,6 +42,8 @@ int main() {
        float area = 1200.25;
        double PIB = 300.50;
        int Numero_de_Pontos_Turisticos = 30;
+       float Densidade_Populacional = Populacao / area;
+       float PIB_per_Capita = PIB * 1000000000 / Populacao; 
 
        printf("Estado: %c \n", Estado);
 
@@ -50,6 +58,10 @@ int main() {
        printf("PIB: %.2lf bilhoes de reais \n", PIB);
 
        printf("Numero de Pontos Turisticos: %d \n", Numero_de_Pontos_Turisticos);
+
+       printf("Densidade Populacional: %.2f hab/km²\n", Densidade_Populacional);
+
+       printf("PIB per Capita: %.2f reais\n", PIB_per_Capita);
    
       
 getchar(); // Aguarda a entrada do usuário
